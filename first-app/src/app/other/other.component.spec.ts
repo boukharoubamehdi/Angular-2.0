84c75052b9de0521481ff8073171c14c17b09460
boukharoubamehdi <boukharouba.mehdi@gmail.com>
2016-10-07 16:24:37 -0400

    other component added

---
+++ first-app/src/app/other/other.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { OtherComponent } from './other.component';
+
+describe('Component: Other', () => {
+  it('should create an instance', () => {
+    let component = new OtherComponent();
+    expect(component).toBeTruthy();
+  });
+});
