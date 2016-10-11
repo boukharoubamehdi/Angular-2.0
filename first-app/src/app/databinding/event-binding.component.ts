848009af44fb9a0a9822073e4f4c973cae4186a7
boukharoubamehdi <boukharouba.mehdi@gmail.com>
2016-10-10 20:13:24 -0400

    More Information on Property & Event Binding

--- first-app/src/app/databinding/event-binding.component.ts
+++ first-app/src/app/databinding/event-binding.component.ts
@@ -10,7 +10,12 @@ import {Component, EventEmitter, Output} from '@angular/core';
 export class EventBindingComponent {
 
   // I'm using @Output() in order to make the custom event clicked listenable outside of this component (event-binding-comp)
- @Output() clicked = new EventEmitter<string>(); // i created an event object.
+  //used it for the Binding Custom Events example.
+ //@Output() clicked = new EventEmitter<string>(); // i created an event object.
+
+  //outside of this component this event is called : clickable but in inside is called clicked.
+  //but the best practice is to use the same name (same logic with @Input().
+  @Output('clickable') clicked = new EventEmitter<string>(); // i created an event object.
 
   onclicked(){
     //alert("It worked!") // used this for the Event Binding example
--- first-app/src/app/databinding/property-binding.component.ts
+++ first-app/src/app/databinding/property-binding.component.ts
@@ -7,9 +7,11 @@ import {Component,Input} from '@angular/core';
    {{resulat}}
    
   `,
-  styles: []
+  styles: [],
+  inputs:['result'] // We can use multiple names we want to use. but the best practice is to use @Input('')
 })
 export class PropertyBindingComponent{
    //this will make this result property  bindable from outside
-  @Input() resulat: number = 0;
+  // resulat: number = 0; // we dont need the @Input() if we use the nputs:['result']  logic
+  @Input()resulat: number = 0;
 }
