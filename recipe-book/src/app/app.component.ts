97535a7fcd74f5d7a3dc6da45fef217a8da6ff0f
boukharoubamehdi <boukharouba.mehdi@gmail.com>
2016-10-11 18:03:45 -0400

    Creating Recipe-Book Project

---
+++ recipe-book/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
