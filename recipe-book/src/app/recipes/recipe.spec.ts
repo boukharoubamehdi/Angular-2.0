d5e6f155458dcb170ae808a7d2526cd92bab856f
boukharoubamehdi <boukharouba.mehdi@gmail.com>
2016-10-12 16:24:44 -0400

    Adding Recipe Model & List Component

---
+++ recipe-book/src/app/recipes/recipe.spec.ts
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+
+import { addProviders, async, inject } from '@angular/core/testing';
+import {Recipe} from './recipe';
+
+describe('Recipe', () => {
+  it('should create an instance', () => {
+    expect(new Recipe()).toBeTruthy();
+  });
+});
