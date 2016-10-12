b306118ef619498ccd12d9aa4784ff9b3cb36ded
boukharoubamehdi <boukharouba.mehdi@gmail.com>
2016-10-12 15:13:15 -0400

    Creating The Recipes Component

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    RecipesComponent,
   ],
   imports: [
     BrowserModule,
