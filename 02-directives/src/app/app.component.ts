0d05750586bcc83c2efdaa74104162e4e7905ced
boukharoubamehdi <boukharouba.mehdi@gmail.com>
2016-10-13 21:15:56 -0400

    Using *ngIf/*ngFor/*ngSwitch and de-sugaring Mechanism

--- 02-directives/src/app/app.component.ts
+++ 02-directives/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private switch = true;
+  private items = [1 ,2 ,3, 4, 5];
+  private value = 10;
 
+  onSwitch(){
+    this.switch = !this.switch;
+  }
 }
