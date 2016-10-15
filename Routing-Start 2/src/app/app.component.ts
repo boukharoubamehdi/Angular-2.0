5159cf0f2cecdc487db04c2a4eac11a746df9f86
boukharoubamehdi <boukharouba.mehdi@gmail.com>
2016-10-15 18:31:04 -0400

    Styling Active Route Links

--- Routing-Start 2/src/app/app.component.ts
+++ Routing-Start 2/src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    templateUrl: 'app.component.html'
+    templateUrl: 'app.component.html',
+    styles:[`
+    .active {
+    border: 1px solid red;
+    }`]
 })
 export class AppComponent {
 }
--- Routing-Start 2/src/app/app.routes.ts
+++ Routing-Start 2/src/app/app.routes.ts
@@ -2,15 +2,6 @@ import {RouterModule,Routes} from "@angular/router";
 import { HomeComponent } from "./home-component.component";
 import { UserComponent } from "./user/user.component";
 import {USER_ROUTES} from "./user/user.routes";
-import {LoadedRouterConfig} from "@angular/router/src/router_config_loader";
-
-// const APP_ROUTES: Routes = [
-//     { path: 'user/:id', component: UserComponent},
-//     { path: 'user/:id', component: UserComponent, children: USER_ROUTES},
-//     // { path:'user', redirectTo : '/user/1', pathMatch: 'full'},
-//     { path: '', component: HomeComponent },
-//     { path:'**', redirectTo : '/user/1', pathMatch: 'full'}
-// ];
 
 const APP_ROUTES: Routes = [
     { path:'user', redirectTo : '/user/1', pathMatch: 'full'},
