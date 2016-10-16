36d3efa9848b90190c89a9d99de2fefb8b3e64a0
boukharoubamehdi <boukharouba.mehdi@gmail.com>
2016-10-16 16:58:11 -0400

    Extracting Recipe Data from Route Params

--- recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,9 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import { Recipe } from '../recipe';
 import { ShoppingListService } from "../../shopping-list/shopping-list.service";
+import {ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs";
+import {RecipeService} from "../recipe.service";
 
 
 @Component({
@@ -8,18 +11,37 @@ import { ShoppingListService } from "../../shopping-list/shopping-list.service";
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent implements OnInit {
+export class RecipeDetailComponent implements OnInit, OnDestroy {
   // Later on i'll fetch this through routing.
   //For now ill fetch this through property binding.
-  @Input() selectedRecipe: Recipe; // creating a property named selectedRecipe
+  //@Input() selectedRecipe: Recipe; // creating a property named selectedRecipe
 
-  constructor(private shoppingListService : ShoppingListService) { }
+  //through routing:
+  selectedRecipe: Recipe;
+  private subscription: Subscription;
+  private recipeIndex : number;
+
+  constructor(private shoppingListService : ShoppingListService, private route: ActivatedRoute,
+  private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+      (params:any) =>{
+        this.recipeIndex = params['id'];
+        this.selectedRecipe = this.recipeService.getRecipe(this.recipeIndex);
+
+      }
+
+    );
+
   }
 
   onAddToShoppingList(){
     this.shoppingListService.addItems(this.selectedRecipe.ingredients);
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -15,4 +15,9 @@ export class RecipeService {
   getRecipes(){
     return this.recipes;
   }
+
+  getRecipe(id: number){
+    return this.recipes[id];
+  }
+
 }
