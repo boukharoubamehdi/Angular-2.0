a8078e69ceba1f11ba29b644e8f5f054d2c324d8
boukharoubamehdi <boukharouba.mehdi@gmail.com>
2016-10-16 16:41:44 -0400

    Navigating between Child Routes

--- recipe-book/src/app/recipes/recipe-list/recipe-item.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import { Recipe } from '../recipe';
 
 @Component({
@@ -6,13 +6,8 @@ import { Recipe } from '../recipe';
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
-export class RecipeItemComponent implements OnInit {
+export class RecipeItemComponent{
   @Input() recipe: Recipe;
-  recipeId: number;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+  @Input() recipeId: number;
 
 }
