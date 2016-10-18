ab35607dd6c33f1c8924553c3629ce48dc2778e3
boukharoubamehdi <boukharouba.mehdi@gmail.com>
2016-10-18 15:37:43 -0400

    Template Driven Approach From Properties/Input Validations /From State/From Groups

--- Start/src/app/template-driven/template-driven.component.ts
+++ Start/src/app/template-driven/template-driven.component.ts
@@ -4,13 +4,33 @@ import { NgForm } from '@angular/Forms';
 
 @Component({
   selector: 'template-driven',
-  templateUrl: 'template-driven.component.html'
+  templateUrl: 'template-driven.component.html',
+  styles:[`
+    input.ng-invalid  {
+        border: 1px solid red;
+    }    
+`]
 })
 export class TemplateDrivenComponent {
+  user={
+    username:'Mehdi',
+    email: 'bou@gmail.com',
+    password:'test',
+    gender: 'male'
+  };
+
+  genders = [
+      'male',
+      'female'
+  ];
+
 
   onSubmit(form: NgForm) {
     console.log('It works!');
-    console.log(form);
+    //console.log(form);
+    console.log(form.value);
+    //console.log(this.user);
+
 
   }
 }
